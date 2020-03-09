a65a12bceb4f3c5af3c1b9faf5f72df341e68bf7
CristobalPerezDAMW <cristobalperez.dam@gmail.com>
2020-03-09 15:59:29 +0100

    Arreglado cierre de sesión

--- login/cerrar.php
+++ login/cerrar.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 session_destroy();
-unset($_SESSION);
+$params = session_get_cookie_params();
+setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
+$_SESSION = array();
 header('Location: ./');
 ?>
