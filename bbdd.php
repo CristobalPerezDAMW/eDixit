661e7c63d3590522ca3af2b6065a68adb7013aed
CristobalPerezDAMW <cristobalperez.dam@gmail.com>
2020-03-30 12:06:00 +0200

    Datos actualizados para la nueva BBDD

--- bbdd.php
+++ bbdd.php
@@ -1,14 +1,16 @@
 <?php
+/*
 $BBDD = (object) array(
     'usuario' => 'usuario_dixit',
     'servidor' => 'localhost',
     'contra' => 'jy8-YBk*WV..DVM',
     'bbdd' => 'db_dixit'
 );
-// $BBDD = (object) array(
-//     'usuario' => 'cristobal',
-//     'servidor' => 'localhost',
-//     'contra' => 'cristobal',
-//     'bbdd' => 'cristobal'
-// );
+*/
+$BBDD = (object) array(
+    'usuario' => 'cristobal',
+    'servidor' => 'localhost',
+    'contra' => 'cristobal',
+    'bbdd' => 'cristobal'
+);
 ?>
