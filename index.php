7bda87bcf64a62de57e3c954ed1392af7ba4dcb9
CristobalPerezDAMW <cristobalperez.dam@gmail.com>
2020-05-21 17:20:15 +0200

    pagina indice final

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 require("cabecera.php");
 ?>
 
-<div class="container p-5">
-    <h1>Efectivamente, aquí no hay nada todavía, pero usa el menú</h1>
-</div>
-<div class="container-fluid bg-danger text-danger-text pt-3 pb-2 mt-1 mb-5">
-    <p class="mensaje_importante">Para probar el juego es posible que sea necesaria la intervención de Cristóbal.<br>También es posible que haya demasiado poco juego que probar.</p>
-</div>
-<div class="container">
-    <div class="row">
+<div class="container p-5 indice">
+    <div class="row salas_publicas">
         <div class="col-12">
-            <a class="btn btn-primary" href="juego" role="button">Probar el Juego</a>
+            <h2>Partida Pública</h2>
+        </div>
+        <div class="col-md-6">
+            <button>Buscar Salas</button>
+        </div>
+        <div class="col-md-6">
+            <button>Crear una Sala</button>
+        </div>
+    </div>
+    <div class="row salas_privadas">
+        <div class="col-12">
+            <h2>Partida Privada</h2>
+        </div>
+        <div class="col-md-6">
+            <button>Buscar Salas</button>
+        </div>
+        <div class="col-md-6">
+            <button>Crear una Sala</button>
         </div>
     </div>
 </div>
