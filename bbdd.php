b33ea6c13cf8d4492bd46435ef8bdb00cf0c558c
CristobalPerezDAMW <cristobalperez.dam@gmail.com>
2020-06-03 10:54:11 +0200

    Eliminación de datos antiguos comentados

--- bbdd.php
+++ bbdd.php
@@ -1,20 +1,4 @@
 <?php
-/*
-$BBDD = (object) array(
-    'usuario' => 'usuario_dixit',
-    'servidor' => 'localhost',
-    'contra' => 'jy8-YBk*WV..DVM',
-    'bbdd' => 'db_dixit'
-);
-
-$BBDD = (object) array(
-    'usuario' => 'cristobal',
-    'servidor' => 'localhost',
-    'contra' => 'cristobal',
-    'bbdd' => 'cristobal'
-);
-*/
-
 $BBDD = (object) array(
     'usuario' => 'cristobal',
     'servidor' => 'localhost',
